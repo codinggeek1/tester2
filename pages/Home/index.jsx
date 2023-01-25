65e3a2823012c34e3f11f79c10a5ccf2d59349ab
PARDHIV VARMA <118751512+codinggeek1@users.noreply.github.com>
2023-01-25 22:40:00 +0530

    Update index.jsx

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -31,9 +31,6 @@ const home = () => {
     <div className='main'>
         <div className="overlay"></div>
         <video src={videoBg} autoPlay loop muted />
-        <div className="content">
-            <h1>Welcome</h1>
-            <p>To my site.</p>
         </div>
     </div>
   )
